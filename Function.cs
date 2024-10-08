084e3151a428f9dfd210e5895e9f44507ec2907b
Hrestonosec <egor.sheva2000@gmail.com>
2024-10-08 15:59:44 +0300

    first commit

---
+++ Function.cs
@@ -0,0 +1,159 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using System.Net;
+using System.Runtime.Intrinsics.X86;
+using System.Text.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace getChats;
+
+public class Function
+{
+    private readonly AmazonDynamoDBClient _client;
+    private readonly DynamoDBContext _context;
+
+    public Function()
+    {
+        _client = new AmazonDynamoDBClient();
+        _context = new DynamoDBContext(_client);
+    }
+
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var userId = request.QueryStringParameters["userId"];
+
+        //Запит на отримання кількості результатів на сторінку
+        request.QueryStringParameters.TryGetValue("pageSize", out var pageSizeString);
+        int.TryParse(pageSizeString, out var pageSize);
+        pageSize = pageSize == 0 ? 50 : pageSize;
+
+        request.QueryStringParameters.TryGetValue("nextChatId", out var nextChatId);
+
+        //Отримання всі чатів вказаного користувача і токена наступної сторінки
+        (List<Chat> chats, string nextPageToken) = await GetAllChats(userId, pageSize, nextChatId);
+
+        var result = chats.Select(chat => new GetAllChatsResponseItem(chat)).ToList();
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            Headers = new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Access-Control-Allow-Origin", "*" }
+        },
+            Body = JsonSerializer.Serialize(new
+            {
+                Chats = result,
+                NextChatId = nextPageToken
+            })
+        };
+    }
+
+    private async Task<(List<Chat>, string)> GetAllChats(string userId, int pageSize, string nextChatId)
+    {
+        var user1Query = new QueryOperationConfig
+        {
+            IndexName = "user1-updatedDt-index",
+            KeyExpression = new Expression
+            {
+                ExpressionStatement = "user1 = :user" + (string.IsNullOrEmpty(nextChatId) ? "" : " AND chatId > :nextChatId"),
+                ExpressionAttributeValues = new Dictionary<string, DynamoDBEntry>
+            {
+                { ":user", userId }
+            }
+            },
+            Limit = pageSize
+        };
+
+        if (!string.IsNullOrEmpty(nextChatId))
+        {
+            user1Query.KeyExpression.ExpressionAttributeValues[":nextChatId"] = nextChatId;
+        }
+
+        var user1Search = _context.FromQueryAsync<Chat>(user1Query);
+        var user1Results = await user1Search.GetNextSetAsync();
+
+        var user2Query = new QueryOperationConfig
+        {
+            IndexName = "user2-updatedDt-index",
+            KeyExpression = new Expression
+            {
+                ExpressionStatement = "user2 = :user" + (string.IsNullOrEmpty(nextChatId) ? "" : " AND chatId > :nextChatId"),
+                ExpressionAttributeValues = new Dictionary<string, DynamoDBEntry>
+            {
+                { ":user", userId }
+            }
+            },
+            Limit = pageSize
+        };
+
+        if (!string.IsNullOrEmpty(nextChatId))
+        {
+            user2Query.KeyExpression.ExpressionAttributeValues[":nextChatId"] = nextChatId;
+        }
+
+        var user2Search = _context.FromQueryAsync<Chat>(user2Query);
+        var user2Results = await user2Search.GetNextSetAsync();
+
+        //Поєднання результатів із запитів по user 1 і 2, і обрізання за розміром сторінки
+        var combinedResults = user1Results.Concat(user2Results)
+                                          .OrderBy(chat => chat.UpdateDt)
+                                          .Take(pageSize)
+                                          .ToList();
+
+        //Отримання токена наступної сторінки
+        string nextPageToken = combinedResults.LastOrDefault()?.ChatId;
+
+        return (combinedResults, nextPageToken);
+    }
+
+
+
+    [DynamoDBTable("chats-db")]
+    public class Chat
+    {
+        [DynamoDBHashKey("chatId")]
+        public string ChatId { get; set; }
+
+        [DynamoDBRangeKey("updatedDt")]
+        public long UpdateDt { get; set; }
+
+        [DynamoDBProperty("user1")]
+        public string User1 { get; set; }
+
+        [DynamoDBProperty("user2")]
+        public string User2 { get; set; }
+
+        public override string? ToString()
+        {
+            return $"{ChatId} {UpdateDt} {User1} {User2}";
+        }
+    }
+
+    public class GetAllChatsResponseItem
+    {
+        public string ChatId { get; set; }
+        public long UpdateDt { get; set; }
+        public string User1 { get; set; }
+        public string User2 { get; set; }
+
+        public GetAllChatsResponseItem()
+        {
+
+        }
+        public GetAllChatsResponseItem(Chat chat)
+        {
+            this.ChatId = chat.ChatId;
+            this.UpdateDt = chat.UpdateDt;
+            this.User1 = chat.User1;
+            this.User2 = chat.User2;
+        }
+    }
+}
---
+++ main.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region  = "eu-north-1"
+  profile = "default"
+}
+
+data "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_getChats"  # Використовуйте існуючу роль
+}
+
+resource "aws_iam_role_policy" "dynamodb_policy" {
+  name   = "dynamodb_access"
+  role   = data.aws_iam_role.lambda_exec_role.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:GetItem"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "getChats" {
+  filename         = "bin/release/net8.0/getChats.zip"
+  function_name    = "getChats"
+  role             = data.aws_iam_role.lambda_exec_role.arn
+  handler          = "getChats::getChats.Function::FunctionHandler"
+  runtime          = "dotnet8"
+  memory_size      = 512
+  timeout          = 30
+  architectures    = ["x86_64"]
+}
+
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = "gatChats"
+  description = "API Gateway for getChats Lambda function"
+}
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "chats"
+}
+
+resource "aws_api_gateway_method" "lambda_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.getChats.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.getChats.invoke_arn
+}
